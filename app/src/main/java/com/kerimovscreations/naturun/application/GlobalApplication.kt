16837e69da8bc042f6e363ec53497e4471d34abd
Karim Karimov <kerimovscreations@gmail.com>
2019-10-26 13:10:22 +0200

    Initial commit

---
+++ app/src/main/java/com/kerimovscreations/naturun/application/GlobalApplication.kt
@@ -0,0 +1,12 @@
+package com.kerimovscreations.naturun.application
+
+import androidx.multidex.MultiDexApplication
+import com.google.firebase.FirebaseApp
+
+class GlobalApplication : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
